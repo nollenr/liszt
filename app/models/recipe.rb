88fa19f009da2cde2c2710d4258a216ac20153d5
Ron <nollenr@gmail.com>
2014-02-07 08:18:02 -0800

    Convert and Tesseract are currently working from pdf->JPG->txt

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -12,10 +12,56 @@ class Recipe < ActiveRecord::Base
   def ocrize_the_recipe
     sleep 5
     # set up the input and output file names and the tesseract system command
+    convert_output_file_name   = 'recipeApp' + SecureRandom.hex(10)
+    convert_output_dir_name    = '/tmp'
+    convert_output_file_ext    = 'JPG'
+    convert_density_setting    = '300'
+    convert_depth_setting      = '8'
     tesseract_output_file_name = 'recipeApp' + SecureRandom.hex(10)
     tesseract_output_dir_name  = '/tmp'
     tesseract_output_file_ext  = 'txt'
-    tesseract_input_file = pre_process_attachment.path
+    tesseract_input_file       = pre_process_attachment.path
+    
+    ########################
+    # Remember to delete the intermediate jpg file from the /tmp directory
+    # Save the intermediate (convert file) so that if there are problems I can see them
+    ########################
+        
+    # If the attachment is a pdf, the first thing I have to do is convert it to a JPG
+    exit_status_Open3 = nil
+    if /\.pdf$/i.match(pre_process_attachment.path)
+      system_command = 'convert -density ' + convert_density_setting + ' ' + pre_process_attachment.path + ' -depth ' + convert_depth_setting + ' -append ' + convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext
+      Delayed::Worker.logger.debug "********** system command is: #{system_command}"
+      Open3.popen3(system_command) do |stdin, stdout, stderr, wait_thr|
+        exit_status_Open3 = wait_thr.value
+      end
+      unless ( /exit 0$/.match(exit_status_Open3.to_s) )
+        mydata = "Processing of PDF attachment \"" + pre_process_attachment_path + "\" failed.\n"
+        mydata += "Output file destination: \"" + convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext + "\"\n"
+        mydata += "Exit status: \"" + exit_status_Open3.to_s + "\n\""
+        mydata += "System command: \"" + system_command + "\"\n"
+        mydata += "ID: " + id.to_s + "\n\n"
+        mydata += "Process output:\n"
+        stdout.each do |line|
+          mydata += line
+        end
+        stderr.each do |line|
+          mydata += line
+        end
+        update_attribute(:attachment_as_text, mydata)
+        return false
+      end
+      # Process was successful, but does the convert outputfile exist?
+      if File::exists?(convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext)
+        # update_attribute(:intermediate_process_attachment, File.open(tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext,'r'))
+        tesseract_input_file = convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext
+      else
+        # Update the text attribute that the convert process returned successfully, but the file did not exist
+        return false
+      end
+    end
+    
+    # Input file should now be a JPG regardless of the original file type.
     system_command = 'tesseract ' + tesseract_input_file  + ' ' + tesseract_output_dir_name + '/' + tesseract_output_file_name
     Delayed::Worker.logger.debug "********** system command is: #{system_command}"
     
@@ -28,7 +74,7 @@ class Recipe < ActiveRecord::Base
       # If the return code is not zero, then capture stdout and stderr and quit.
       exit_status_Open3 = wait_thr.value
       unless ( /exit 0$/.match(exit_status_Open3.to_s) )
-        mydata = "Processing of attachment \"" + tesseract_input_file + "\" failed.\n"
+        mydata = "Processing of JPEG attachment \"" + tesseract_input_file + "\" failed.\n"
         mydata += "Output file destination: \"" + tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext + "\"\n"
         mydata += "Exit status: \"" + exit_status_Open3.to_s + "\n\""
         mydata += "System command: \"" + system_command + "\"\n"
@@ -62,6 +108,9 @@ class Recipe < ActiveRecord::Base
       
       # delete the temporary file once it is uploaded to the application
       File.delete(tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext)
+    else
+      # Update the text attribute that the convert process returned successfully, but the file did not exist
+      return false
     end # end if File::exists?
   end # end ocrize_the_recipe
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204231844) do
+ActiveRecord::Schema.define(version: 20140206221013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20140204231844) do
     t.integer  "post_process_attachment_file_size"
     t.datetime "post_process_attachment_updated_at"
     t.text     "attachment_as_text"
+    t.datetime "processing_start_time"
+    t.datetime "processing_end_time"
   end
 
   add_index "recipes", ["name"], name: "index_recipes_on_name", unique: true, using: :btree
