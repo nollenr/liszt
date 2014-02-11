069d7e157e87011d1fadf43593fddf14155ecc6a
Ron <nollenr@gmail.com>
2014-02-11 07:07:28 -0800

    Intermediate commit.  Uploading txt, image (jpg, tiff, png), and pdf documents is rough, but working.

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -3,10 +3,15 @@ module RecipesHelper
   #used in the recipe model by the ocrize function
   def log_activity_to_record(id, message)
     # Update informational debugging data
-    existingdata = Recipe.select(:attachment_processing_output).find(id)
-    message = existingdata.attachment_processing_output.to_s.empty? ? message : existingdata.attachment_processing_output.to_s + "\n" + message
-    Recipe.update_all({attachment_processing_output: message},{id: id})
+    existing_data = Recipe.select(:attachment_processing_output).find(id)
+    existing_data.attachment_processing_output.nil? ? message : message = existing_data.attachment_processing_output + "\n" + message
+    Recipe.where(id: id).update_all(attachment_processing_output: message)
     # End information debugging data
+  end
+  
+  def end_ocrize_processing(id, status)
+    update_attribute(:attachment_processing_successful, status)
+    update_attribute(:attachment_processing_endtime, Time.now)
   end
   
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -19,7 +19,6 @@ class Recipe < ActiveRecord::Base
     # Update the record to reflect whne processing started
     update_attribute(:attachment_processing_starttime, Time.now)
 
-    sleep 5
     # set up the input and output file names and the tesseract system command
     convert_output_file_name   = 'recipeAppJPG' + SecureRandom.hex(10)
     convert_output_dir_name    = '/tmp'
@@ -42,25 +41,23 @@ class Recipe < ActiveRecord::Base
     
     ######################################################################
     #                                                                    #
-    # Below is the convert of a PDF to JPG                               #
+    # First check to see if the incoming file i9s type text              #
     #                                                                    #
     ######################################################################
     
     if original_attachment_content_type == 'text/plain'
       # Update informational debugging data
-      existingdata = Recipe.select(:attachment_processing_output).find(id)
-      mydata = existingdata.attachment_processing_output.to_s
-      mydata += "\n" + Time.now.to_s + ' ' + ' Moving attachment to post_processing_attacment and eliminating original_attachment.'
-      Delayed::Worker.logger.debug "********** mydata: #{mydata}"
-      update_attribute(:attachment_processing_output, mydata)
+      message = Time.now.to_s + ' ' + ' Moving attachment to post_processing_attacment.'
+      log_activity_to_record(id, message)
       # End information debugging data
       
       update_attribute(:post_process_attachment, File.open(original_attachment.path,'r'))
-      update_attribute(:original_attachment, nil)
+      # I don't think I EVER want to get rid of the original attachment
+      # or at least give that ability (responsibility?) to the end user.
+      #update_attribute(:original_attachment, nil)
       
-      update_attribute(:attachment_as_text, mydata)
-      update_attribute(:attachment_processing_successful, true)
-      update_attribute(:attachment_processing_endtime, Time.now)
+      # end processing
+      end_ocrize_processing(id, true)
       return true
     end 
     
@@ -76,10 +73,8 @@ class Recipe < ActiveRecord::Base
       Delayed::Worker.logger.debug "********** system command is: #{system_command}"
 
       # Update informational debugging data
-      existingdata = Recipe.select(:attachment_processing_output).find(id)
-      mydata = existingdata.attachment_processing_output.to_s
-      mydata += "\n" + Time.now.to_s + ' ' + 'Attempting PDF conversion with command: ' + system_command
-      update_attribute(:attachment_processing_output, mydata)
+      message = Time.now.to_s + ' ' + 'Attempting PDF conversion with command: ' + system_command
+      log_activity_to_record(id, message)
       # End information debugging data
       
       # Run the convert command
@@ -89,41 +84,40 @@ class Recipe < ActiveRecord::Base
       
       # Processing unsuccessful according to the return code
       unless ( /exit 0$/.match(exit_status_Open3.to_s) )
-        mydata = Time.now.to_s + " Processing of PDF attachment \"" + original_attachment_path + "\" failed.\n"
-        mydata += "Output file destination: \"" + convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext + "\"\n"
-        mydata += "Exit status: \"" + exit_status_Open3.to_s + "\n\""
-        mydata += "System command: \"" + system_command + "\"\n"
-        mydata += "ID: " + id.to_s + "\n\n"
-        mydata += "Process output:\n"
+        message = Time.now.to_s + " Processing of PDF attachment \"" + original_attachment_path + "\" failed.\n"
+        message += "Output file destination: \"" + convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext + "\"\n"
+        message += "Exit status: \"" + exit_status_Open3.to_s + "\n\""
+        message += "System command: \"" + system_command + "\"\n"
+        message += "ID: " + id.to_s + "\n\n"
+        message += "Process output:\n"
         stdout.each do |line|
-          mydata += line
+          message += line
         end
         stderr.each do |line|
-          mydata += line
+          message += line
         end
-        update_attribute(:attachment_processing_output, mydata)
-        update_attribute(:attachment_processing_successful, false)
+        log_activity_to_record(id, message)
+        end_ocrize_processing(id, false)
         return false
       end # End Processing unsuccessful according to the return code
       # Process was successful, but does the convert outputfile exist?
       if File::exists?(convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext)
         # Update informational degugging data
-        existingdata = Recipe.select(:attachment_processing_output).find(id)
-        mydata = existingdata.attachment_processing_output.to_s
-        mydata += "\n" + Time.now.to_s + ' ' + 'Successful conversion of PDF to JPG.  Exit status is: ' + exit_status_Open3.to_s
-        update_attribute(:attachment_processing_output, mydata)
+        message = Time.now.to_s + ' ' + 'Successful conversion of PDF to JPG.  Exit status is: ' + exit_status_Open3.to_s
+        log_activity_to_record(id, message)
         # End informational debugging data
-        
+
+        # Attach the intermediate file        
         update_attribute(:intermediate_process_attachment, File.open(convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext,'r'))
         tesseract_input_file = convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext
       else
         # Update informational degugging data
-        existingdata = Recipe.select(:attachment_processing_output).find(id)
-        mydata = existingdata.attachment_processing_output.to_s
-        mydata += "\n" + Time.now.to_s + ' Convert process reported as successful, but output file "' + convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext + '" reported not to exist.'
-        mydata += "\n" + Time.now.to_s + ' Exit status of convert was ' + exit_status_Open3.to_s
-        update_attribute(:attachment_processing_output, mydata)
+        mesasge = Time.now.to_s + ' Convert process reported as successful, but output file "' + convert_output_dir_name + '/' + convert_output_file_name + '.' + convert_output_file_ext + '" reported not to exist.'
+        message += "\n" + Time.now.to_s + ' Exit status of convert was ' + exit_status_Open3.to_s
+        log_activity_to_record(id, message)
+        end_ocrize_processing(id, false)
         # End informational debugging data
+        
         return false
       end
     end #End processing the file if the original was a pdf.  
@@ -139,6 +133,11 @@ class Recipe < ActiveRecord::Base
     system_command = 'tesseract ' + tesseract_input_file  + ' ' + tesseract_output_dir_name + '/' + tesseract_output_file_name
     Delayed::Worker.logger.debug "********** system command is: #{system_command}"
     
+    # Update informational debugging data
+    message = Time.now.to_s + ' ' + 'Attempting JPG conversion to text with command: ' + system_command
+    log_activity_to_record(id, message)
+    # End information debugging data
+    
     # run the system command and capture stdout, sterr and status
     exit_status_Open3 = nil
     Open3.popen3(system_command) do |stdin, stdout, stderr, wait_thr|
@@ -148,20 +147,20 @@ class Recipe < ActiveRecord::Base
       # If the return code is not zero, then capture stdout and stderr and quit.
       exit_status_Open3 = wait_thr.value
       unless ( /exit 0$/.match(exit_status_Open3.to_s) )
-        mydata = "Processing of JPEG attachment \"" + tesseract_input_file + "\" failed.\n"
-        mydata += "Output file destination: \"" + tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext + "\"\n"
-        mydata += "Exit status: \"" + exit_status_Open3.to_s + "\n\""
-        mydata += "System command: \"" + system_command + "\"\n"
-        mydata += "ID: " + id.to_s + "\n\n"
-        mydata += "Process output:\n"
+        message = Time.now.to_s + "Processing of JPEG attachment \"" + tesseract_input_file + "\" failed.\n"
+        message += "Output file destination: \"" + tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext + "\"\n"
+        message += "Exit status: \"" + exit_status_Open3.to_s + "\n\""
+        message += "System command: \"" + system_command + "\"\n"
+        message += "ID: " + id.to_s + "\n\n"
+        message += "Process output:\n"
         stdout.each do |line|
-          mydata += line
+          message += line
         end
         stderr.each do |line|
-          mydata += line
+          message += line
         end
-        update_attribute(:attachment_processing_output, mydata)
-        update_attribute(:attachment_processing_successful, false)
+        log_activity_to_record(id, message)
+        end_ocrize_processing(id, false)
         return false
       end # end check for exit status
     end #end the Open3.popen3
@@ -170,30 +169,31 @@ class Recipe < ActiveRecord::Base
     # assuming that the exit status of tesseract was 0 (which is successful)
     Delayed::Worker.logger.debug "********** checking to see if file exists"
     if File::exists?(tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext)
-      mydata=''
+      log_activity_to_record(id, Time.now.to_s + " Convertion to text file was successful.  Exit status was: " + exit_status_Open3.to_s + " Attaching text output.")
+      text_output = ''
       File.foreach(tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext) do |line|
         Delayed::Worker.logger.debug "********** #{line}"
-        mydata += line
+        text_output += line
       end # End the File::foreach
       
-      update_attribute(:attachment_as_text, mydata)
+      update_attribute(:attachment_as_text, text_output)
       
       # upload the text file into the rails application
       update_attribute(:post_process_attachment, File.open(tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext,'r'))
       
       # delete the temporary file once it is uploaded to the application
       File.delete(tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext)
-      update_attribute(:attachment_processing_endtime, Time.now)
-      update_attribute(:attachment_processing_successful, true)
+      
+      log_activity_to_record(id, Time.now.to_s + " Processing complete.  Exiting with success")
+      end_ocrize_processing(id, true)
     else
       # Update the text attribute that the convert process returned successfully, but the file did not exist
       update_attribute(:attachment_processing_successful, false)
       # Update informational degugging data
-      existingdata = Recipe.select(:attachment_processing_output).find(id)
-      mydata = existingdata.attachment_processing_output.to_s
-      mydata += "\n" + Time.now.to_s + ' Tesseract process reported as successful, but output file "' + tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext + '" reported not to exist.'
-      mydata += "\n" + Time.now.to_s + ' Exit status of convert was ' + exit_status_Open3.to_s
-      update_attribute(:attachment_processing_output, mydata)
+      message = "\n" + Time.now.to_s + ' Tesseract process reported as successful, but output file "' + tesseract_output_dir_name + '/' + tesseract_output_file_name + '.' + tesseract_output_file_ext + '" reported not to exist.'
+      message += "\n" + Time.now.to_s + ' Exit status of convert was ' + exit_status_Open3.to_s
+      log_activity_to_record(id, message)
+      end_ocrize_processing(id, false)
       # End informational debugging data
       return false
     end # end if File::exists?
